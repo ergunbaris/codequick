ab1b93e71c725fbfe124ca2e5b4250e17753a5ca
bergun <barisergun75@gmail.com>
2015-11-10 20:12:16 +0000

    Stack: optimizations with static nested Node class

--- stack/src/api/Stack.java
+++ stack/src/api/Stack.java
@@ -2,31 +2,30 @@ package api;
 import java.util.NoSuchElementException;
 
 public class Stack<E>{
-    private E item;
-    private Stack<E> next;
-    private Stack<E> top;
+    private static class StackNode<E>{
+        private final E item;
+        private StackNode<E> next;
+        private StackNode(E item){
+            this.item = item;
+        }
+        
+    }
+    private StackNode<E> top;
  
     public Stack(){}
-    private Stack(E item){
-        this.item = item;
-    }
     public void push(E item){
         if(null == item){
             throw new IllegalArgumentException("pushing null element in stack not allowed!");
         }
-        Stack<E> curNode = new Stack<E>(item);
-        if(null == top){
-            top = curNode;
-        }else{
-            curNode.next = top;
-            top = curNode;
-        }
+        StackNode<E> curNode = new StackNode<E>(item);
+        curNode.next = top;
+        top = curNode;
     }
     public E pop(){
         if(null == top){
             throw new NoSuchElementException();
         }
-        Stack<E> temp = top;
+        StackNode<E> temp = top;
         top = top.next;
         temp.next = null;
         return temp.item;
