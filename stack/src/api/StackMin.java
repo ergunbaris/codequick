a70d1b264a54039fd1d82ccc472bef03312148ba
bergun <barisergun75@gmail.com>
2015-11-11 21:08:02 +0000

    StackMin retrospective

--- stack/src/api/StackMin.java
+++ stack/src/api/StackMin.java
@@ -1,6 +1,17 @@
 package api;
 import java.util.NoSuchElementException;
 
+/*
+    RETROSPECTIVE:
+    - As shown below extending class can define a more restricted Generic type
+    this is logical because StackMin can be casted to Stack but vice versa not valid
+    so when casting from StackMin to Stack E which is Comparable can also be an Object
+
+    - For better memory model we used another stack to keep track of mins.
+    - should always use compareTo for comparing generics.
+    
+*/
+
 public class StackMin<E extends Comparable> extends Stack<E>{
     Stack<E> minStack = new Stack<>();
     public StackMin(){}
