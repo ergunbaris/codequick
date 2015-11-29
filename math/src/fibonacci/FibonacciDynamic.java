f12a6dd03534196d9d71ac989cd01075f06ce72c
bergun <barisergun75@gmail.com>
2015-11-29 12:15:46 +0000

    math: bottom up dynamic fibonacci

--- math/src/fibonacci/FibonacciDynamic.java
+++ math/src/fibonacci/FibonacciDynamic.java
@@ -6,18 +6,27 @@ public class FibonacciDynamic
     {
       int max = Integer.parseInt(args[0]);
       long [] fibSerie = new FibonacciDynamic().getFibonacciSeriesUpto(max);
-      for(int i=0; i<fibSerie.length; i++ )
+      printFib(fibSerie);
+      long [] fibSerie2 = new FibonacciDynamic().getBottomUpFibonacciSeriesUpto(max);
+      printFib(fibSerie2);
+      long fibSerieRes = new FibonacciDynamic().getBottomUpFibonacciResult(max);
+      System.out.printf("fibonacci for %d is=%d%n",max, fibSerieRes);
+    
+    }
+  public static void printFib(long [] fibSerie)
+    {
+    for(int i=0; i<fibSerie.length; i++ )
+      {
+      if(i == 0)
         {
-        if(i == 0)
-          {
-          System.out.printf("%d", fibSerie[i]);
-          }
-        else
-          {
-          System.out.printf(",%d", fibSerie[i]);
-          }
+        System.out.printf("%d", fibSerie[i]);
         }
-        System.out.printf("%n");
+      else
+        {
+        System.out.printf(",%d", fibSerie[i]);
+        }
+      }
+      System.out.printf("%n");
     }
   public long [] getFibonacciSeriesUpto(int max)
     {
@@ -37,5 +46,32 @@ public class FibonacciDynamic
         fibonaccis[nth] = generateFibonacci(fibonaccis,nth-1) + generateFibonacci(fibonaccis,nth-2);
       }
     return fibonaccis[nth];
+    }
+  public long [] getBottomUpFibonacciSeriesUpto(int max)
+    {
+    long [] fibonaccis = new long[max+1];
+    fibonaccis[0] = 0;
+    fibonaccis[1] = 1;
+    for(int i=2;i<fibonaccis.length;i++)
+      {
+      fibonaccis[i] = fibonaccis[i-1] + fibonaccis[i-2];
+      }
+    return fibonaccis;
+    }
+  public long getBottomUpFibonacciResult(int max)
+    {
+    if(max == 0 || max == 1)
+      {
+        return max;
+      }
+    long a = 0;
+    long b = 1;
+    for(int i=2;i<max;i++)
+      {
+      long c = a+b;
+      a = b;
+      b = c;  
+      }
+    return a+b;
     }
   }
