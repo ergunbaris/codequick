c8352e716133fb3fc40c8d52b699c19b6c17738f
bergun <baris.ergun@seagate.com>
2015-12-11 09:31:45 +0000

    euler: sum of primes

--- math/src/prime/EratosthenesAll.java
+++ math/src/prime/EratosthenesAll.java
@@ -3,12 +3,39 @@ package prime;
 import java.util.List;
 import java.util.LinkedList;
 
+/*
+Retrospective: WALK THROUGH YOUR CODE CAREFULLY
+*/
+
 public class EratosthenesAll
 {
   public static void main (String ... args)
   {
     int n = Integer.parseInt(args[0]);
-    System.out.printf("%d th prime is %d%n", n, new EratosthenesAll().getNthPrimeNumber(n));
+    System.out.printf("sum of prime under %d is %d%n", n, new EratosthenesAll().findSumOfPrimesUnder(n));
+    //System.out.printf("%d th prime is %d%n", n, new EratosthenesAll().getNthPrimeNumber(n));
+  }
+  public long findSumOfPrimesUnder(int n)
+  {
+  long sum = 0L;
+  boolean [] nonprimes = new boolean[n+1];
+  int prime = 2;
+  sum += prime;
+  int sqrtN = (int)Math.sqrt(n);
+  while(prime<=sqrtN)
+    {
+    flagNonPrimes(nonprimes, prime);
+    prime = getNextPrime(nonprimes,prime);
+    sum += prime;
+    }
+  for(int i=prime+1;i<n;i++)
+    {
+    if(!nonprimes[i])
+      {
+      sum+=i;
+      }
+    }
+    return sum;
   }
   public int getNthPrimeNumber(int n)
   {
@@ -25,12 +52,9 @@ public class EratosthenesAll
   }
   public void flagNonPrimes(boolean [] nonprimes, int prime)
     {
-    if(prime < 45000)
-    {
-      for(int i=prime*prime;i<nonprimes.length;i += prime)
-        {
-        nonprimes[i] = true;
-        }
+    for(int i=prime*prime;i<nonprimes.length;i += prime)
+      {
+      nonprimes[i] = true;
       }
     }
   public int getNextPrime(boolean [] nonprimes, int curPrime)
