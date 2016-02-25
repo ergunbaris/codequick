35eab3fd1d830a24b2c5333643d45baf5909ac5e
bergun <barisergun75@gmail.com>
2016-02-25 21:59:03 +0000

    euler: reciprocal cycles improved

--- euler/src/reciprocal/Cycles.java
+++ euler/src/reciprocal/Cycles.java
@@ -3,37 +3,66 @@ import java.util.*;
 
 public class Cycles
   {
-  private final LinkedList<Integer> [] reciprocalLists;
+  private final ReciprocalNumber [] reciprocalNumbers;
   private int maxCycleIndex = 1;
   public static void main (String ... args)
     {
     int max = Integer.parseInt(args[0]);
-    System.out.println(new Cycles(max).getMaximumReciprocalNumber());
+    Cycles cycles = new Cycles(max);
+    System.out.println(cycles.getMaximumReciprocalNumber());
+    cycles.displayAllReciprocals();
     }
   public Cycles(int max)
     {
-    reciprocalLists = new LinkedList[max];
-    findCycles(reciprocalLists);
+    reciprocalNumbers = new ReciprocalNumber[max];
+    findCycles(reciprocalNumbers);
+    }
+  public void displayAllReciprocals()
+    {
+    for (int i = 0; i < reciprocalNumbers.length; i++)
+      {
+      if (null == reciprocalNumbers[i])
+        {
+        continue;
+        }
+      System.out.printf("%d -> %s", i, "0.");
+      for (int j = 0; j < reciprocalNumbers[i].fractionals.size(); j++)
+        {
+        if (reciprocalNumbers[i].reciprocalIndex -1  == j)
+          {
+          System.out.printf("%s","(");
+          }
+        System.out.printf("%d", reciprocalNumbers[i].fractionals.get(j));
+        }
+      System.out.printf("%s%n", ")");
+      }
     }
   public int getMaximumReciprocalNumber()
     {
     return maxCycleIndex;
     }
-  private void findCycles(LinkedList<Integer> [] reciprocalLists)
+  private void findCycles(ReciprocalNumber [] reciprocalNumbers)
     {
     int maxCycleCnt = 0;
-    for (int i = 1; i < reciprocalLists.length; i++)
+    for (int i = 1; i < reciprocalNumbers.length; i++)
       {
-      int [] remainders = new int[reciprocalLists.length];
+      Map<Integer,Integer> remaindersMap = new HashMap<>();
+      Map<Integer,Integer> resultsMap = new HashMap<>();
       int numerator = 1;
       int remainder = 0;
       int result = 0;
       int index = 1;
+      LinkedList<Integer> fractionals = null;
       while ((remainder = ((numerator*10)%i)) != 0)
         {
-        if (remainders[remainder] != 0)
+        result = ((numerator*10)/i);
+        if (remaindersMap.containsKey(remainder) && 
+            fractionals.get(remaindersMap.get(remainder)-1) == result)
           {
-          int cycleCount = reciprocalLists[i].size() - remainders[remainder] + 1;
+          reciprocalNumbers[i] = new ReciprocalNumber(fractionals, 
+                                                      remaindersMap.get(remainder));
+          int cycleCount = reciprocalNumbers[i].fractionals.size() 
+                           - reciprocalNumbers[i].reciprocalIndex + 1;
           if (maxCycleCnt < cycleCount)
             {
             maxCycleCnt = cycleCount;
@@ -43,21 +72,28 @@ public class Cycles
           }
         else
           {
-          remainders[remainder] = index;
+          remaindersMap.put(remainder, index);
           index++;
           }
-        result = ((numerator*10)/i);
-        if (reciprocalLists[i] == null)
+        if (fractionals == null)
           {
-          reciprocalLists[i] = new LinkedList<>();
+          fractionals = new LinkedList<>();
           }
-        reciprocalLists[i].addLast(result);
+        fractionals.addLast(result);
         numerator = remainder;
         }
-      if (remainder == 0)
-        {
-        reciprocalLists[i] = null;
-        }
+      }
+    }
+  private static class ReciprocalNumber
+    {
+    private final List<Integer> fractionals;
+    private final int reciprocalIndex;
+    
+    ReciprocalNumber(LinkedList<Integer> fractionals, 
+                     int reciprocalIndex)
+      {
+      this.fractionals = fractionals;
+      this.reciprocalIndex = reciprocalIndex;
       }
     }
   }
