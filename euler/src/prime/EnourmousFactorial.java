a5fc838132d79abbe456a0d5b122c1e09c42a84d
bergun <barisergun75@gmail.com>
2016-08-14 14:36:58 +0100

    euler: enourmous factorial final solution 1-2 seconds

--- euler/src/prime/EnourmousFactorial.java
+++ euler/src/prime/EnourmousFactorial.java
@@ -3,6 +3,11 @@ package prime;
 import java.util.Map;
 import java.util.HashMap;
 
+/* Retrospective
+- Learn math trick behind number of p prime factors of two numbers or factorial of number? can be added up
+- Never trust Math.pow because of rounding errors.
+*/
+
 public class EnourmousFactorial
   {
   private static final long S0 = 290797L;  
@@ -10,57 +15,84 @@ public class EnourmousFactorial
 
   private final int p;
   private final int q;
-  private final long N;
-  private final Eratosthenes sieve;
-  private final Divisors divisors;
+  private final int m;
+  private final long modN;
+  private final Map<Integer, Long> powerCache = new HashMap<>();
   public static void main (String ... args)
     {
     int p = Integer.parseInt(args[0]);
     int q = Integer.parseInt(args[1]);
-    EnourmousFactorial ef = new EnourmousFactorial(p,q);
+    int m = Integer.parseInt(args[2]);
+    EnourmousFactorial ef = new EnourmousFactorial(p,q,m);
     long NF = ef.findNF();    
     System.out.println(NF);
     
     }
   public EnourmousFactorial(int p,
-                            int q)
+                            int q,
+                            int m)
     {
     this.p = p;
     this.q = q;
-    this.N = findN();
-    this.sieve = new Eratosthenes((int)N);
-    this.divisors = new Divisors(this.sieve);
+    this.m = m;
+    modN = findPow(p, m);
     }
 
-  public long findN()
+  public long findNF()
     {
-    long result = 0;
+    long numOfFactors = 0;
     long sn = S0;
-    for (int i = 0; i < this.q; i++)
+    for (int i = 0; i <= this.q; i++)
       {
-      result += (sn % p) * (long) Math.pow(this.p, i);
-      sn = (sn * sn) % 50515093L;
+      int pow = i;
+      if (i > this.m)
+        {
+        pow = this.m;
+        }
+      long Ti = sn % p;
+      sn = (sn * sn) % S_MOD;      
+
+      if (Ti == 0)
+        {
+        continue;
+        }
+      numOfFactors += findNumOfFactors(Ti, pow);
+      if (numOfFactors >= modN)
+        {
+        numOfFactors %= modN;
+        }
       }
-    return result;
+    return numOfFactors % modN;
     }
 
-   public long findNF()
+   public long findNumOfFactors(long Ti,
+                                int pPow)
       {
-      long N = findN();
-      System.out.println(N);
-      Map<Long, Integer> primeDivs = this.divisors.getFactorialsPrimeDivisors(N);
-      displayPrimeMap(primeDivs);
-      return this.divisors.findTheNumberOfFactors(primeDivs);
+      long numFactors = 0;
+      for (int i = pPow - 1; i >= 0; i--)
+        {
+        numFactors += (Ti * findPow(p, i));
+        if (numFactors >= modN)
+          {
+          numFactors %= modN;
+          }
+        }
+      return numFactors % modN;
+      }
+    private long findPow(long number,
+                         int pow)
+      {
+      if (powerCache.containsKey(pow))
+        {
+        return powerCache.get(pow);
+        }
+      long res = 1;
+      for (int i = 0; i < pow; i++)
+        {
+        res *= number;
+        }
+      powerCache.put(pow, res);
+      return res;
       }
 
-    private void displayPrimeMap(Map<Long, Integer> primeMap)
-      {
-      for(Map.Entry<Long, Integer> entry: primeMap.entrySet())
-	{
-	System.out.printf("primeDiv=%d, exp=%d%n",
-			  entry.getKey(),
-			  entry.getValue());
-	}
-
-      }
   }
